5d9f6c5457e244c67c146bdae194a02617bd4422
Matija Osrečki <matija.osrecki@gmail.com>
2022-02-22 10:44:57 +0100

    ✏️ change utils.ts cookie wording

--- apps/backend/src/lib/utils.ts
+++ apps/backend/src/lib/utils.ts
@@ -1,11 +1,11 @@
-export const cookiesToObj = (cookies: string) =>
-  cookies
+export const cookieToObject = (cookie: string) =>
+  cookie
     ?.split('; ')
     .filter(Boolean)
     .map((s) => s.split('='))
     .reduce((acc, [k, v]) => ({ ...acc, [k]: v }), {}) ?? {};
 
-export const getFromCookie = (cookies: string, field: string) => {
-  const obj = cookiesToObj(cookies);
+export const getFromCookie = (cookie: string, field: string): string | null => {
+  const obj = cookieToObject(cookie);
   return obj[field] ?? null;
 };
