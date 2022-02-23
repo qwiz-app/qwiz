5cf41e60164a347c0613205b5895b1b03058c28a
Mislav <mislavjc@gmail.com>
2022-02-23 17:33:31 +0100

    🩹 fix waring for external api request

--- apps/frontend/pages/api/[...all].ts
+++ apps/frontend/pages/api/[...all].ts
@@ -1,6 +1,12 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import httpProxyMiddleware from 'next-http-proxy-middleware';
 
+export const config = {
+  api: {
+    externalResolver: true,
+  },
+};
+
 const proxy = (req: NextApiRequest, res: NextApiResponse) => {
   return httpProxyMiddleware(req, res, {
     target: process.env.BACKEND_URL,
