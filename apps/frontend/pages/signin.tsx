9b019879d6569e3295bf283a372d453ddbbe3da8
Matija Osrečki <matija.osrecki@gmail.com>
2022-02-26 17:31:13 +0100

    💄 design signin page with peeps

--- apps/frontend/pages/signin.tsx
+++ apps/frontend/pages/signin.tsx
@@ -1,9 +1,18 @@
 import { Group } from '@mantine/core';
+import Peep from 'assets/peep2.svg';
+import PeepDark from 'assets/peep2-dark.svg';
 import AuthLayout from 'components/layout/AuthLayout';
 import ProviderButton, { ProviderId } from 'components/UI/ProviderButton';
+import { useAppColorscheme } from 'hooks/colorscheme';
 import { GetServerSideProps } from 'next';
 import { BuiltInProviderType } from 'next-auth/providers';
-import { ClientSafeProvider, getProviders, LiteralUnion, signIn } from 'next-auth/react';
+import {
+  ClientSafeProvider,
+  getProviders,
+  LiteralUnion,
+  signIn,
+} from 'next-auth/react';
+import Image from 'next/image';
 import React, { useEffect } from 'react';
 
 export const getServerSideProps: GetServerSideProps = async () => {
@@ -23,24 +32,47 @@ type Props = {
 };
 
 const SignInPage = ({ redirectUrl, providers }: Props) => {
+  const { isDark } = useAppColorscheme();
+
   useEffect(() => {
-    console.log('providers :>> ', providers);
+    console.table(providers);
   }, [providers]);
 
   const signInHandler = (providerId: ProviderId) => {
     signIn(providerId, { callbackUrl: redirectUrl });
   };
 
+  const illustration = isDark ? PeepDark : Peep;
+
   return (
-    <Group direction="column" spacing={8}>
-      {Object.values(providers).map((provider) => (
-        <ProviderButton
-          key={provider.id}
-          id={provider.id as ProviderId}
-          name={provider.name}
-          onClick={signInHandler}
-        />
-      ))}
+    <Group
+      noWrap
+      align="center"
+      position="center"
+      spacing={100}
+      sx={() => ({ marginLeft: '-10vw' })}
+    >
+      <div style={{ flex: '0 0 60vh' }}>
+        <Image src={illustration} alt="city" objectFit="contain" />
+      </div>
+      <Group position="center">
+        <Group
+          direction="column"
+          spacing={8}
+          align="center"
+          position="center"
+          sx={() => ({ width: 'fit-content' })}
+        >
+          {Object.values(providers).map((provider) => (
+            <ProviderButton
+              key={provider.id}
+              id={provider.id as ProviderId}
+              name={provider.name}
+              onClick={signInHandler}
+            />
+          ))}
+        </Group>
+      </Group>
     </Group>
   );
 };
