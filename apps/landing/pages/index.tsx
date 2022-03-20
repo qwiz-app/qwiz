ae09b1a5698531e47360576f561d1aed2c80af77
Mislav <mislavjc@gmail.com>
2022-03-20 17:56:00 +0100

    🎉 init landing page app

---
+++ apps/landing/pages/index.tsx
@@ -0,0 +1,11 @@
+import styles from 'styles/index.module.scss';
+
+export const Index = () => {
+  return (
+    <div className={styles.page}>
+      <h1>Welcome to landing!</h1>
+    </div>
+  );
+}
+
+export default Index;
