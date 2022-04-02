bdf365dc2b9c00fce6a537815243f8ca05d49bf3
Marian Babic <mbabic6@tvz.hr>
2022-04-02 14:05:35 +0200

    :bug: fix logo link image ref

--- apps/frontend/components/Nav/NavbarLogo.tsx
+++ apps/frontend/components/Nav/NavbarLogo.tsx
@@ -3,14 +3,21 @@ import LogoWhite from 'assets/logo/qwiz-white.svg';
 import { useAppColorscheme } from 'hooks/colorscheme';
 import Image from 'next/image';
 import Link from 'next/link';
+import { forwardRef } from 'react';
 
-export const NavbarLogo = () => {
+const LogoImage = forwardRef(function LogoImage() {
   const { isDark } = useAppColorscheme();
   const logo = isDark ? LogoWhite : LogoDark;
 
   return (
+    <Image src={logo} alt="logo" objectFit="contain" width={64} height={64} />
+  );
+});
+
+export const NavbarLogo = () => {
+  return (
     <Link href="/" passHref>
-      <Image src={logo} alt="logo" objectFit="contain" width={64} height={64} />
+      <LogoImage />
     </Link>
   );
 };
