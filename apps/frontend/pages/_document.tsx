da1f20c07d8e62447835977ea5904a9278b0ab65
Mislav <mislavjc@gmail.com>
2022-04-14 15:18:51 +0200

    📈 add Plausible to frontend

--- apps/frontend/pages/_document.tsx
+++ apps/frontend/pages/_document.tsx
@@ -22,6 +22,11 @@ export default class _Document extends Document {
             href="https://fonts.googleapis.com/css2?family=Manrope:wght@300;400;500;600;700;800&display=swap"
             rel="stylesheet"
           />
+          <script
+            defer
+            data-domain={process.env.NEXT_PUBLIC_PLAUSIBLE_DOMAIN}
+            src="https://plausible.qwiz.party/js/plausible.js"
+          />
         </Head>
         <body>
           <Main />
