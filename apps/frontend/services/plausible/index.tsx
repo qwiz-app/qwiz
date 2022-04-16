40bb3e3246ff5974f7edcaa774f44c76e8aac9fc
Mislav <mislavjc@gmail.com>
2022-04-16 19:50:23 +0200

    📈 update metrics for Plausible hooks

--- apps/frontend/services/plausible/index.tsx
+++ apps/frontend/services/plausible/index.tsx
@@ -4,17 +4,28 @@ import http from 'services/http';
 
 interface Visitors {
   results: {
+    bounce_rate: {
+      value: number;
+    };
+    pageviews: {
+      value: number;
+    };
+    visit_duration: {
+      value: number;
+    };
     visitors: {
       value: number;
     };
   };
 }
 
+const metrics = 'metrics=visitors,pageviews,bounce_rate,visit_duration';
+
 export const fetchVisitors = async (url: string) => {
   const encodedUrl = encodeURIComponent(url);
 
   const response = await http.get<Visitors>(
-    `${config.plausible.url}/api/v1/stats/aggregate?site_id=${config.plausible.domain}&period=6mo&filters=event:page%3D%3D${encodedUrl}`,
+    `${config.plausible.url}/api/v1/stats/aggregate?site_id=${config.plausible.domain}&${metrics}&filters=event:page%3D%3D${encodedUrl}`,
     {
       headers: {
         Authorization: `Bearer ${config.plausible.apiKey}`,
@@ -28,12 +39,15 @@ interface TopPages {
   results: {
     page: string;
     visitors: number;
+    bounce_rate: number;
+    pageviews: number;
+    visit_duration: number;
   }[];
 }
 
 export const fetchTopPages = async () => {
   const response = await http.get<TopPages>(
-    `${config.plausible.url}/api/v1/stats/breakdown?site_id=${config.plausible.domain}&period=6mo&property=event:page`,
+    `${config.plausible.url}/api/v1/stats/breakdown?site_id=${config.plausible.domain}&${metrics}&property=event:page`,
     {
       headers: {
         Authorization: `Bearer ${config.plausible.apiKey}`,
