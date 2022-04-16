d4dc1bdefc6ba27e408beb4e588b171fc2a6285f
Mislav <mislavjc@gmail.com>
2022-04-16 19:19:03 +0200

    ♻️ switch to config for envs

--- apps/frontend/services/plausible/index.tsx
+++ apps/frontend/services/plausible/index.tsx
@@ -1,4 +1,5 @@
 import { parseData } from 'lib/axios';
+import config from 'lib/config';
 import http from 'services/http';
 
 interface Visitors {
@@ -13,10 +14,10 @@ export const fetchVisitors = async (url: string) => {
   const encodedUrl = encodeURIComponent(url);
 
   const response = await http.get<Visitors[]>(
-    `${process.env.NEXT_PUBLIC_PLAUSIBLE_URL}/api/v1/stats/aggregate?site_id=${process.env.NEXT_PUBLIC_PLAUSIBLE_DOMAIN}&period=6mo&filters=event:page%3D%3D${encodedUrl}`,
+    `${config.plausible.url}/api/v1/stats/aggregate?site_id=${config.plausible.domain}&period=6mo&filters=event:page%3D%3D${encodedUrl}`,
     {
       headers: {
-        Authorization: `Bearer ${process.env.NEXT_PUBLIC_PLAUSIBLE_API_KEY}`,
+        Authorization: `Bearer ${config.plausible.apiKey}`,
       },
     }
   );
@@ -32,10 +33,10 @@ interface TopPages {
 
 export const fetchTopPages = async () => {
   const response = await http.get<TopPages>(
-    `${process.env.NEXT_PUBLIC_PLAUSIBLE_URL}/api/v1/stats/breakdown?site_id=${process.env.NEXT_PUBLIC_PLAUSIBLE_DOMAIN}&period=6mo&property=event:page`,
+    `${config.plausible.url}/api/v1/stats/breakdown?site_id=${config.plausible.domain}&period=6mo&property=event:page`,
     {
       headers: {
-        Authorization: `Bearer ${process.env.NEXT_PUBLIC_PLAUSIBLE_API_KEY}`,
+        Authorization: `Bearer ${config.plausible.apiKey}`,
       },
     }
   );
