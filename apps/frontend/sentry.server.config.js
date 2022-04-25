76420a9755d8777cdc85bd9d62b7e81686c29650
Mislav <mislavjc@gmail.com>
2022-04-25 12:48:00 +0200

    🔧 disable sentry in dev mode

--- apps/frontend/sentry.server.config.js
+++ apps/frontend/sentry.server.config.js
@@ -7,11 +7,14 @@ import * as Sentry from '@sentry/nextjs';
 const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
 
 Sentry.init({
-  dsn: SENTRY_DSN || 'https://4990d2c38f2242f4a4e0802b7baf9fbd@o1083688.ingest.sentry.io/6340895',
+  dsn:
+    SENTRY_DSN ||
+    'https://4990d2c38f2242f4a4e0802b7baf9fbd@o1083688.ingest.sentry.io/6340895',
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1.0,
   // ...
   // Note: if you want to override the automatic release value, do not set a
   // `release` value here - use the environment variable `SENTRY_RELEASE`, so
   // that it will also get attached to your source maps
+  enabled: process.env.NODE_ENV !== 'development',
 });
