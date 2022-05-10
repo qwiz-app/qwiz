6982c7d351934ea205b434968208c346503649a4
Matija Osrečki <matija.osrecki@gmail.com>
2022-05-10 19:36:48 +0200

    ✨ optimistically delete quiz

--- apps/frontend/hooks/api/quiz/use-quiz-delete.tsx
+++ apps/frontend/hooks/api/quiz/use-quiz-delete.tsx
@@ -1,3 +1,4 @@
+import { Quiz } from '@prisma/client';
 import { useQueryClient, useMutation } from 'react-query';
 import { deleteQuiz } from 'services/api/quiz';
 
@@ -5,8 +6,24 @@ export const useQuizDelete = (id: string) => {
   const queryClient = useQueryClient();
 
   return useMutation(() => deleteQuiz(id), {
+    onMutate: async () => {
+      await queryClient.cancelQueries(['quizzes']);
+
+      const previousQuizzes = queryClient.getQueryData('quizzes') as Quiz[];
+
+      queryClient.setQueryData(
+        'quizzes',
+        previousQuizzes.filter((quiz) => quiz.id !== id)
+      );
+
+      return { previousQuizzes };
+    },
+    onError(err, variables, context: { previousQuizzes: Quiz[] }) {
+      if (context?.previousQuizzes) {
+        queryClient.setQueryData<Quiz[]>('quizzes', context.previousQuizzes);
+      }
+    },
     onSuccess: () => {
-      // TODO: add optimistic update
       queryClient.invalidateQueries('quizzes');
     },
   });
--- apps/frontend/hooks/api/quiz/use-quiz-update.tsx
+++ apps/frontend/hooks/api/quiz/use-quiz-update.tsx
@@ -10,22 +10,17 @@ export const useQuizUpdate = (quizId: string) => {
     {
       // TODO: OPTIMISTIC UPDATE NAME - could be done better, by id?
       onMutate: async (newQuiz) => {
-        // Cancel any outgoing refetches (so they don't overwrite our optimistic update)
         // await queryClient.cancelQueries(['quizzes', quizId]);
         await queryClient.cancelQueries(['quizzes']);
 
-        // Snapshot the previous value
         const previousQuizzes = queryClient.getQueryData('quizzes') as Quiz[];
 
-        // TODO: updatedAt not updated immediately beacuse no access to response yet
         const updatedQuizzes = previousQuizzes.map((quiz) =>
           quiz.id !== quizId ? quiz : { ...quiz, ...newQuiz }
         );
 
-        // Optimistically update to the new value
         queryClient.setQueryData('quizzes', updatedQuizzes);
 
-        // Return a context object with the snapshotted value
         return { previousQuizzes };
       },
       onError(err, variables, context: { previousQuizzes: Quiz[] }) {
