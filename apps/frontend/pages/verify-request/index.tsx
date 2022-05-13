ac74cea3c6d189e7be737c4ff813f1b53e0c4594
Matija Osrečki <matija.osrecki@gmail.com>
2022-05-13 20:53:30 +0200

    💄 style verify email request page

--- apps/frontend/pages/verify-request/index.tsx
+++ apps/frontend/pages/verify-request/index.tsx
@@ -1,28 +1,71 @@
-import { Box, Button, Text } from '@mantine/core';
+import { Box, Button, Group, Paper, Stack, Text, Title } from '@mantine/core';
+import peep from 'assets/peeps/peep-verify.svg';
 import AuthLayout from 'components/Layouts/AuthLayout';
+import { useBreakpoints } from 'hooks/breakpoints';
+import { useAppColorscheme } from 'hooks/colorscheme';
+import Image from 'next/image';
+import { PaperPlaneRight, SmileySad } from 'phosphor-react';
 
 const VerifyRequest = () => {
+  const { isDark, theme } = useAppColorscheme();
+  const { matches } = useBreakpoints();
+
+  const resendEmailHandler = () => {
+    console.log('resend email');
+  };
+
   return (
-    <Box
-      sx={(theme) => ({
-        backgroundColor:
-          theme.colorScheme === 'dark'
-            ? theme.colors.dark[6]
-            : theme.colors.gray[0],
+    <Paper
+      sx={(t) => ({
         textAlign: 'center',
-        padding: theme.spacing.xl,
-        borderRadius: theme.radius.md,
+        padding: t.spacing.xl,
+        borderRadius: t.radius.md,
+        transform: 'translateY(-4vh)',
       })}
     >
-      <h1>Check your email</h1>
-      <Text color="gray" mb={12}>
-        A sign in link has been sent to your email address.
-      </Text>
-      <Text color="blue" mb={6}>
-        Don&apos;t see the email yet?
-      </Text>
-      <Button>Resend verification email</Button>
-    </Box>
+      <Stack align="center" justify="space-between">
+        <Box>
+          <Image
+            src={peep}
+            objectFit="contain"
+            width={500}
+            height={500}
+            alt="Peep"
+          />
+          <Stack spacing={12}>
+            <Title order={matches.max.sm ? 3 : 2}>
+              <Group position="center">Check your email</Group>
+            </Title>
+            <Text color="gray" mb={12}>
+              A sign-in link has been sent to your email address.
+            </Text>
+          </Stack>
+        </Box>
+        <Stack mt={32}>
+          <Text
+            sx={(t) => ({
+              color: t.colors.violet[isDark ? 4 : 6],
+              transform: 'translateX(-4px)',
+            })}
+          >
+            <Group position="center" spacing={4}>
+              <SmileySad
+                size={22}
+                color={theme.colors.violet[isDark ? 4 : 6]}
+                weight="duotone"
+              />
+              Don&apos;t see our email yet?
+            </Group>
+          </Text>
+          <Button size="md" variant="light" onClick={resendEmailHandler}>
+            <Group spacing={8}>
+              Resend email
+              <PaperPlaneRight weight="duotone" />
+            </Group>
+          </Button>
+        </Stack>
+      </Stack>
+    </Paper>
   );
 };
 
