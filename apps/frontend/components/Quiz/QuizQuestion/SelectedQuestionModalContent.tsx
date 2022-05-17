f1e933caefb0d492855f1b8ba3b577d10a7b607b
Matija Osrečki <matija.osrecki@gmail.com>
2022-05-17 21:09:24 +0200

    ✨ fix up question modal contents

--- apps/frontend/components/Quiz/QuizQuestion/SelectedQuestionModalContent.tsx
+++ apps/frontend/components/Quiz/QuizQuestion/SelectedQuestionModalContent.tsx
@@ -6,10 +6,9 @@ import {
   Paper,
   SimpleGrid,
   Stack,
-  Title,
+  Title
 } from '@mantine/core';
 import { QuestionElementType } from '@prisma/client';
-import React from 'react';
 import { QuestionWithContentAndOwnerAndCategoriesAndMode } from 'types/question';
 
 interface Props {
@@ -31,37 +30,84 @@ export const SelectedQuestionModalContent = ({ question }: Props) => {
 
   return (
     <Box>
-     
-      {hasImageElements && (
-        <Paper withBorder p="md" radius="md">
-          <Badge size="md">Images</Badge>
-          <Stack  align="start">
-            <SimpleGrid cols={2}>
-              {imageElements.map((elem) => (
-                <Paper
-                  key={elem.id}
-                  radius="sm"
-                  sx={() => ({ overflow: 'hidden' })}
-                >
-                  <Image src={elem.content} alt="question image" />
-                </Paper>
-              ))}
-            </SimpleGrid>
-          </Stack>
-        </Paper>
-      )}
+      <Box>
+        <Title order={5} align="left" mt={20} mb={24}>
+          Question contents
+        </Title>
+        <Stack classNames={classes.sectionElementsWrapper} spacing="lg">
+          {hasTextElements && (
+            <Paper
+              withBorder
+              p="md"
+              pt="lg"
+              radius="md"
+              className={classes.sectionElementsWrapper}
+            >
+              <Badge size="md" className={classes.badge} variant="filled">
+                Textual
+              </Badge>
+              <Stack align="start">
+                {textElements.map((elem) => (
+                  <Paper key={elem.id} radius="sm">
+                    <Title order={6}>{elem.content}</Title>
+                  </Paper>
+                ))}
+              </Stack>
+            </Paper>
+          )}
+
+          {hasImageElements && (
+            <Paper
+              withBorder
+              p="md"
+              radius="md"
+              className={classes.sectionElementsWrapper}
+            >
+              <Badge size="md" className={classes.badge} variant="filled">
+                Images
+              </Badge>
+              <Stack align="start">
+                <SimpleGrid cols={2}>
+                  {imageElements.map((elem) => (
+                    <Paper
+                      key={elem.id}
+                      radius="sm"
+                      sx={() => ({ overflow: 'hidden' })}
+                    >
+                      <Image src={elem.content} alt="question image" />
+                    </Paper>
+                  ))}
+                </SimpleGrid>
+              </Stack>
+            </Paper>
+          )}
+        </Stack>
+      </Box>
+      <Box>
+        <Title order={5} align="left" mt={20} mb={24}>
+          Accepted answers
+        </Title>
+        <Stack classNames={classes.sectionElementsWrapper} spacing="lg">
+          Answers
+        </Stack>
+      </Box>
     </Box>
   );
 };
 
 const useStyles = createStyles((theme) => ({
   sectionWrapper: {
+    width: '100%',
+  },
+
+  sectionElementsWrapper: {
     position: 'relative',
   },
 
   badge: {
     position: 'absolute',
-    top: '-25%',
-    left: '10px',
+    top: -9,
+    left: 16,
+    zIndex: 10,
   },
 }));
