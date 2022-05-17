d83eb51c415f998e7a0f6f3191f2186fcb71a17f
Mislav <mislavjc@gmail.com>
2022-05-17 22:55:05 +0200

    🚧 add upload file modal

--- apps/frontend/components/UI/FloatingQuizMenu.tsx
+++ apps/frontend/components/UI/FloatingQuizMenu.tsx
@@ -1,10 +1,16 @@
-import { Button, createStyles, Group, Paper } from '@mantine/core';
+import { Button, createStyles, Group, Paper, Modal } from '@mantine/core';
+import { QuestionElementType } from '@prisma/client';
 import { useQuestionContentCreate } from 'hooks/api/slide';
+import { useFileUpload } from 'hooks/use-flle-upload';
 import { useRouter } from 'next/router';
 import { TextT, Image as ImageIcon } from 'phosphor-react';
+import { useEffect, useState } from 'react';
+import { FileUpload } from './FileUpload';
 
 export const FloatingQuizMenu = () => {
   const router = useRouter();
+
+  const [showUploadModal, setShowUploadModal] = useState(false);
 
   const { slideId } = router.query;
   const { mutate } = useQuestionContentCreate(slideId as string);
@@ -12,28 +18,17 @@ export const FloatingQuizMenu = () => {
   const { classes } = useStyles();
 
   const handleTextClick = () => {
-    mutate({
-      content: 'Edit me',
-      type: 'TEXT',
-      question: {
-        create: {},
-      },
-      quizSlideElement: {
-        create: [
-          {
-            quizSlideId: slideId as string,
-            point: {
-              create: {
-                x: 0.5,
-                y: 0.5,
-                width: 100,
-                height: 16,
-              },
-            },
-          },
-        ],
-      },
-    });
+    mutate(
+      generateMutateData({
+        type: QuestionElementType.TEXT,
+        content: 'Edit me',
+        slideId,
+      })
+    );
+  };
+
+  const handleImageClick = () => {
+    setShowUploadModal(true);
   };
 
   return (
@@ -50,11 +45,17 @@ export const FloatingQuizMenu = () => {
         <Button
           leftIcon={<ImageIcon size={18} weight="duotone" />}
           variant="light"
+          onClick={handleImageClick}
           color="indigo"
         >
           Add image
         </Button>
       </Group>
+      <UploadImageModal
+        opened={showUploadModal}
+        onClose={() => setShowUploadModal(false)}
+        slideId={slideId}
+      />
     </Paper>
   );
 };
@@ -70,3 +71,70 @@ const useStyles = createStyles((theme) => ({
     width: 'max-content',
   },
 }));
+
+interface GenerateMutateDataProps {
+  type: QuestionElementType;
+  content: string;
+  slideId: string | string[];
+}
+
+const generateMutateData = ({
+  type,
+  content,
+  slideId,
+}: GenerateMutateDataProps) => {
+  return {
+    content,
+    type,
+    question: {
+      create: {},
+    },
+    quizSlideElement: {
+      create: [
+        {
+          quizSlideId: slideId as string,
+          point: {
+            create: {
+              x: 0.5,
+              y: 0.5,
+              width: 300,
+              height: 300,
+            },
+          },
+        },
+      ],
+    },
+  };
+};
+
+const UploadImageModal = ({ opened, onClose, slideId }) => {
+  const { selectFile, uploadFile, uploadingStatus, url, file } =
+    useFileUpload();
+
+  const { mutate } = useQuestionContentCreate(slideId as string);
+
+  useEffect(() => {
+    if (uploadingStatus === 'UPLOADED' && opened) {
+      mutate(
+        generateMutateData({
+          type: QuestionElementType.IMAGE,
+          content: url,
+          slideId,
+        })
+      );
+      onClose();
+    }
+  }, [url]);
+
+  useEffect(() => {
+    if (file) {
+      uploadFile();
+    } 
+  }, [file]);
+
+  return (
+    <Modal opened={opened} onClose={onClose}>
+      <FileUpload selectFile={selectFile} />
+    </Modal>
+  );
+};
