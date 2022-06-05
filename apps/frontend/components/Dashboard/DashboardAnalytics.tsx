0d29a3d2298111cd1ada37f709327b7a2447d3f6
Matija Osrečki <matija.osrecki@gmail.com>
2022-06-05 17:59:25 +0200

    ✨ write less fake dashboard analytics

--- apps/frontend/components/Dashboard/DashboardAnalytics.tsx
+++ apps/frontend/components/Dashboard/DashboardAnalytics.tsx
@@ -4,21 +4,21 @@ export const DashboardAnalytics = (props) => {
   const data = [
     {
       title: 'Page views',
-      stats: '456,133',
+      stats: '2,493',
       description:
         '24% more than in the same month last year, 33% more that two years ago',
     },
     {
       title: 'New users',
-      stats: '2,175',
+      stats: '463',
       description:
         '13% less compared to last month, new user engagement up by 6%',
     },
     {
-      title: 'Completed orders',
-      stats: '1,994',
+      title: 'Completed reservations',
+      stats: '112',
       description:
-        '1994 orders were completed this month, 97% satisfaction rate',
+        '112 atendees reserved events this month, 97% satisfaction rate',
     },
   ];
 
